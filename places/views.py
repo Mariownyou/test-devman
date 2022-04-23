e88f6ba6f00534d5196f44f4d2708326e5a4ee74
mariownyou <smj510black@gmail.com>
2022-04-23 17:08:16 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
 
+from places.models import Place
+
 
 def home(request):
     # map_template = get_template('map/index.html')
-    return render(request, 'map/index.html', {})
+    places = Place.objects.all()
+    return render(request, 'map/index.html', {'places': places})
