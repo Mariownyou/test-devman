7c70b8e0b9a2651836cc2bd6debf13e838d1d405
mariownyou <smj510black@gmail.com>
2022-04-25 12:41:59 +0300

    Added place preview in admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Place, PlaceImage
 
 
 class ImageInline(admin.TabularInline):
     model = PlaceImage
+    fields = ('image', 'place_image', 'position')
+    readonly_fields = ("place_image",)
+
+    def place_image(self, obj):
+        height = obj.image.height
+        if height > 200:
+            height = 200
+
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width='auto',
+            height=height
+            )
+        )
 
 
 class PlaceAdmin(admin.ModelAdmin):
